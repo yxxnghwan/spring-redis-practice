76ac27f55798a1071cc2840ca41b10115c360fd9
yxxnghwan <younghwan960@gmail.com>
2022-10-08 20:56:42 +0900

    project init

---
+++ src/test/java/alex/practice/springredis/SpringredisApplicationTests.java
@@ -0,0 +1,13 @@
+package alex.practice.springredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringredisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
